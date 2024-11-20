d0b1bede8f59e317baf4c825c47c95cc05262890
kadeem <kingkadeem11@gmail.com>
2024-11-20 14:16:17 -0500

    made corrections to profile handelbars home routes and comment routes

--- db/schema.sql
+++ db/schema.sql
@@ -3,10 +3,3 @@ CREATE DATABASE movie_db;
 
 \c movie_db;
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
-
